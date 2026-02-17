3776a4f85f42dd692a2b3623ec032f66ae87def4
pranav9151 <pranavmahajan9151@gmail.com>
2026-02-17 10:31:31 +0530

    day 2

--- queries/power_user_analysis/08_power_user_analysis.sql
+++ queries/power_user_analysis/08_power_user_analysis.sql
@@ -21,5 +21,26 @@ ORDER BY total_revenue DESC
 LIMIT 100;
 
 
+-- STEP 2 – Define Power Users (Revenue Based) -----------------------------
+
+WITH user_metrics AS (
+    SELECT
+        user_id,
+        SUM(CASE WHEN event_type = 'purchase' THEN price ELSE 0 END) AS total_revenue
+    FROM events_raw
+    GROUP BY user_id
+)
+
+SELECT
+    user_id,
+    total_revenue
+FROM user_metrics
+WHERE total_revenue > (
+    SELECT PERCENTILE_CONT(0.90) 
+    WITHIN GROUP (ORDER BY total_revenue)
+    FROM user_metrics
+)
+ORDER BY total_revenue DESC;
+
 
 
