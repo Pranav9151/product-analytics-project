ae295016a4918491c65602f3708ca3df365d63a8
pranav9151 <pranavmahajan9151@gmail.com>
2026-02-17 10:30:36 +0530

    Step 1

---
+++ queries/power_user_analysis/08_power_user_analysis.sql
@@ -0,0 +1,25 @@
+-- DAY 8: Power User Analysis
+
+WITH user_metrics AS (
+    SELECT
+        user_id,
+
+        COUNT(*) AS total_events,
+
+        COUNT(DISTINCT user_session) AS total_sessions,
+
+        SUM(CASE WHEN event_type = 'purchase' THEN price ELSE 0 END) AS total_revenue,
+
+        SUM(CASE WHEN event_type = 'purchase' THEN 1 ELSE 0 END) AS total_purchases
+
+    FROM events_raw
+    GROUP BY user_id
+)
+
+SELECT * FROM user_metrics
+ORDER BY total_revenue DESC
+LIMIT 100;
+
+
+
+
