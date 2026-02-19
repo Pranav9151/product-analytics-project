56859d6c897a16c3de1fc80d65c0018e7551f699
pranav9151 <pranavmahajan9151@gmail.com>
2026-02-19 18:36:13 +0530

    step 2

--- queries/growth_metrics/09_growth_metrics.sql
+++ queries/growth_metrics/09_growth_metrics.sql
@@ -6,3 +6,15 @@ SELECT
 FROM events_raw
 GROUP BY DATE(event_time)
 ORDER BY activity_date;
+
+-- STEP 2 – Weekly Active Users (WAU) -----------------------------
+
+SELECT
+    DATE_TRUNC('month', event_time) AS activity_month,
+    COUNT(DISTINCT user_id) AS mau
+FROM events_raw
+GROUP BY DATE_TRUNC('month', event_time)
+ORDER BY activity_month;
+
+
+
