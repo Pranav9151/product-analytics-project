c44d2cbc982964e80ed41edde1e015617fe3d279
pranav9151 <pranavmahajan9151@gmail.com>
2026-02-23 22:10:39 +0530

    Likewise added till step 4

--- queries/data_modeling/13_star_schema.sql
+++ queries/data_modeling/13_star_schema.sql
@@ -13,4 +13,38 @@ FROM events_raw;
 CREATE INDEX idx_dim_date_full_date
 ON dim_date(full_date);
 
+-- STEP 2 – Create User Dimension---------------------------------
+
+CREATE TABLE dim_users AS
+SELECT DISTINCT
+    user_id
+FROM events_raw;
+
+CREATE INDEX idx_dim_users_id
+ON dim_users(user_id);
+
+
+-- STEP 3 – Create Product Dimension---------------------------------
+
+CREATE TABLE dim_product AS
+SELECT DISTINCT
+    product_id,
+    category_code,
+    brand
+FROM events_raw;
+
+CREATE INDEX idx_dim_product_id
+ON dim_product(product_id);
+
+-- STEP 4 – Create Session Dimension---------------------------------
+
+CREATE TABLE dim_session AS
+SELECT DISTINCT
+    user_session,
+    user_id
+FROM events_raw;
+
+CREATE INDEX idx_dim_session_id
+ON dim_session(user_session);
+
 
