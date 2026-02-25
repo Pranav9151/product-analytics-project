77546f1fc6bcd773f59d10b40aaa20a03a486157
pranav9151 <pranavmahajan9151@gmail.com>
2026-02-25 16:11:49 +0530

    added the remaining steps

--- queries/performance/15_partitioning.sql
+++ queries/performance/15_partitioning.sql
@@ -15,3 +15,30 @@ CREATE TABLE fact_events (
 
 -- STEP 3- Create Year-Based Partitions---------------------------------------------------
 
+CREATE TABLE fact_events_2023
+PARTITION OF fact_events
+FOR VALUES FROM ('2023-01-01') TO ('2024-01-01');
+
+CREATE TABLE fact_events_2024
+PARTITION OF fact_events
+FOR VALUES FROM ('2024-01-01') TO ('2025-01-01');
+
+-- STEP 4 – Insert Data From events_raw----------------------------------------------------------
+
+INSERT INTO fact_events
+SELECT
+    user_id,
+    user_session,
+    product_id,
+    DATE(event_time),
+    event_type,
+    price
+FROM events_raw;
+
+-- STEP 5 - Add Indexes on Partitions----------------------------------------------------------
+
+CREATE INDEX idx_fact_user_date
+ON fact_events(user_id, event_date);
+
+CREATE INDEX idx_fact_product
+ON fact_events(product_id);
